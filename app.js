75f878bf9933f98fcce59470e6c5bcfa929dd5e8
xScoopy <JLeary070@gmail.com>
2021-10-03 14:14:50 -0500

    added get route and tested that we could run server

--- app.js
+++ app.js
@@ -5,3 +5,11 @@ const http = require('http')
 const server = http.createServer(app)
 const { Server } = require('socket.io')
 const io = new Server(server)
+
+app.get('/', (req, res) => {
+    res.sendFile('./static/index.html')
+})
+
+server.listen(3000, ()  => {
+    console.log('Listening for requests!')
+})
