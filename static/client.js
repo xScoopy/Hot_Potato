dee3829877fb6c57fcc66310c393745ca9678789
xScoopy <JLeary070@gmail.com>
2021-10-04 14:25:43 -0500

    added event listener for ready button

--- static/client.js
+++ static/client.js
@@ -1,8 +1,32 @@
-console.log('hello world')
+const e = require("cors");
+
+console.log("hello world");
 
 const socket = io();
 
+let form = document.getElementById("form");
+let input = document.getElementById("email");
+let alerts = document.getElementById("alerts")
+
+//Event Listeners
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  if (input.value) {
+    socket.emit("player_ready", input.value);
+    //store player email somewhere
+    form.style.display = "none"
+  }
+});
+
+//Socker.io
 socket.on("some_event_here", () => {
-    //Event functionality here
-    return 0 //just to not throw errors, can remove when finished with function
-})
+  //Event functionality here
+  return 0; //just to not throw errors, can remove when finished with function
+});
+
+socket.on("player_join", () => {
+  //send a message to alerts
+  //prompt them to enter email
+});
+
+socket.on("player_ready", () => {});
