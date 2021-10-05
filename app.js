1ea51d5015a88f764efda3289faa4cfc68ec46f1
xScoopy <69348168+xScoopy@users.noreply.github.com>
2021-10-05 04:46:12 -0500

    added html and styling for alerts box and email form. Added socket communication for those areas. created button for passing the potato.

--- app.js
+++ app.js
@@ -1,29 +1,35 @@
-const express = require('express')
+const express = require("express");
 const app = express();
 
-const http = require('http')
-const server = http.createServer(app)
-const { Server } = require('socket.io')
-const io = new Server(server)
+const http = require("http");
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
 //We'll store our online users here
-let onlineUsers = {};
+let players = [];
 
+app.use("/static", express.static("./static"));
 
-app.use("/static", express.static('./static'))
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/static/index.html");
+});
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/static/index.html')
-})
+io.on("connection", (socket) => {
+  //initial emissions
+  socket.broadcast.emit("welcome_user");
+  socket.emit("player_join");
 
-io.on('connection', (socket) => {
-    //where our emits will happen
-    // socket.broadcast.emit("player_join")
+  //player ready
+  socket.on("player_ready", (email) => {
+    io.emit("player_ready", email);
+  });
 
-    socket.broadcast.emit("welcome_user")
-    socket.emit("player_join")
-    // player_join and player_ready
-})
+  //disconnect broadcast
+  socket.on("disconnect", () => {
+    io.emit("user_disconnect");
+  });
+});
 
-server.listen(3000, ()  => {
-    console.log('Listening for requests!')
-})
+server.listen(3000, () => {
+  console.log("Listening for requests!");
+});
--- static/client.js
+++ static/client.js
@@ -1,67 +1,49 @@
-
 console.log("hello world");
 
-let socket = io()
+let socket = io();
 
-// let form = document.getElementById("form");
-// let input = document.getElementById("email");
-let alerts = document.getElementById("alerts")
+let form = document.getElementById("form");
+let readyButton = document.getElementById("ready-button");
+let email = document.getElementById("email");
+let alerts = document.getElementById("alerts");
 
-//Event Listeners
-// form.addEventListener("submit", (event) => {
-//   event.preventDefault();
-//   if (input.value) {
-//     socket.emit("player_ready", input.value);
-//     //store player email somewhere
-//     form.style.display = "none"
-//   }
-// });
-
-//Socket.io
-// socket.on("some_event_here", () => {
-//   //Event functionality here
-//   return 0; 
-//   //just to not throw errors, can remove when finished with function
-// });
+// Event Listeners
+form.addEventListener("click", (event) => {
+  event.preventDefault();
+  if (email.value) {
+    socket.emit("player_ready", email.value);
+    //store player email somewhere
+    // form.style.display = "none";
+  }
+});
 
 socket.on("player_join", () => {
   //send a message to alerts
-  let item = document.createElement("li")
-  item.textContent = "A new player has joined"
-  alerts.appendChild(item)
+  let item = document.createElement("li");
+  item.textContent = "A new player has joined";
+  alerts.appendChild(item);
   //prompt them to enter email
-  alert("Please enter your email and ready up!")
+  alert("Please enter your email and ready up!");
 });
 
 socket.on("welcome_user", () => {
-    let item = document.createElement("li")
-    item.textContent = "A new player has joined"
-    alerts.appendChild(item)
-})
+  let item = document.createElement("li");
+  item.textContent = "A new player has joined";
+  alerts.appendChild(item);
+});
 
-// socket.on("disconnect", () => {
-//     let item = document.createElement("li")
-//     item.textContent = "A player has disconnected"
-//     alerts.appendChild(item)
-// })
+socket.on("user_disconnect", () => {
+  let item = document.createElement("li");
+  item.textContent = "A player has disconnected";
+  alerts.appendChild(item);
+});
 
-// // Listen for "new user" socket emits
-// socket.on('new user', (username) => {
-//   //Save the username as key to access the user's socket id
-//   onlineUsers[username] = socket.id;
-//   //Save the username to socket as well. This is important for later.
-//   socket["username"] = username;
-//   console.log(`✋ ${username} has joined the chat! ✋`);
-//   //Send the username to all clients currently connected
-//   io.emit("new user", username);
-// })
-
-// socket.on("player_ready", () => {
-//   //send a message to alerts
-//   let item = document.createElement("li")
-//   item.textContent = "A new player is ready"
-//   alerts.append(item)
-// });
+socket.on("player_ready", (email) => {
+  //send a message to alerts
+  let item = document.createElement("li");
+  item.textContent = email + " is ready!";
+  alerts.append(item);
+});
 
 // socket.on("timer_start", () => {
 //   //send a message to alerts
